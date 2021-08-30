36587a809b47fcfd3178f2f8312667373d6a66df
Trevor Foland <trevorfoland@outlook.com>
2021-08-30 15:54:03 -0400

    Got rid of annoying sign in phrase.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 export default function Home(props) {
     return(
         <div>
-            <p>YoU dId It! YoU sIgNeD iN!</p>
             <Link to="/kitchen">Kitchen</Link>
         </div>
     );
